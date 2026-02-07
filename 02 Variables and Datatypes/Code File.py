f7b7fc29b674e6db484d16b407082a19a1cf11b8
Haroon Khalid <muhammadharoonkhalid786@gmail.com>
2026-02-07 23:35:08 +0500

    Add arithmetic operations and area calculation

--- 02 Variables and Datatypes/Code File.py
+++ 02 Variables and Datatypes/Code File.py
@@ -17,3 +17,21 @@ print(x)
 # Check type of user input
 x = input("Enter number : ")
 print(type(x))
+
+# Perform arithmetic operations
+a = 3
+b = 8
+print(a+b)
+print(a-b)
+print(a*b)
+
+# Calculate area of a rectangle
+length = 10
+width = 6
+print("Area of rectangle is : ", length*width)
+
+# Calculate simple interest
+princple_amount = 500
+interest = 200
+interest_rate = 6
+print("Simple Interest is : ", (princple_amount*interest*interest_rate)/100)
