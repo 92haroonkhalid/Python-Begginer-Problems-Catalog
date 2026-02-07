c46b7122400c6e23ddf08fffd26a874183b38462
Haroon Khalid <muhammadharoonkhalid786@gmail.com>
2026-02-07 23:06:17 +0500

    Create Code File.py

---
+++ 02 Variables and Datatypes/Code File.py
@@ -0,0 +1,19 @@
+# Create variables of all basic data types
+x = None
+y = "Ali"
+z = 23
+a = 5.4
+b = True
+
+# Convert int to float
+x = 7
+x = float(x)
+print(x)
+
+# Convert string to integer
+x = int("123")
+print(x)
+
+# Check type of user input
+x = input("Enter number : ")
+print(type(x))
